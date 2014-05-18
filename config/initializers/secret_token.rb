519885ce78c36402ec7dba59fff26bcb4688c9ae
Prelang Builder <builder@prelang.com>
2014-05-18 01:52:49 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DogeTip::Application.config.secret_key_base = '2dce9f75082f280e81d3a498a8bc37b5ad576608b6dea077a4330f61df834deb092bde1d826ae9e5420ba7b6579e30d1d920f8de5db6fb6b178d9e1035a011c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DogeTip::Application.config.session_store :cookie_store, key: '_doge_tip_session'
